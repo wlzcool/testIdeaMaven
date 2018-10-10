9f7d56131230609a2717d04662817dbf514dc610
ouryoushi <wlzcool@foxmail.com>
2018-10-10 23:04:01 +0800

    test 视图

--- src/main/java/com/zhiguogongfang/controller/DemoController.java
+++ src/main/java/com/zhiguogongfang/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.zhiguogongfang.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,15 +21,22 @@ public class DemoController {
 
         return "Hello Spring";
     }
+
     @RequestMapping(path = "test", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView testModelAndView() {
         logger.info("testModelAndView 方法被调用");
 
-        ModelAndView mv =new ModelAndView();
-        mv.addObject("Message","Hello ModelAndView");
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("Message", "Hello ModelAndView");
         mv.setViewName("/WEB-INF/Views/Demo/welcome.jsp");
         return mv;
     }
 
+    @RequestMapping(path = "testView", method = RequestMethod.GET)
+    public String testView(Model model) {
+        logger.info("testView 方法被调用");
+        model.addAttribute("Message", "hello world");
+        return "/Demo/welcome";
+    }
 }
---
+++ src/main/webapp/WEB-INF/Views/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ouryoushi
+  Date: 2018/10/10
+  Time: 下午10:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${requestScope.Message}00
+</body>
+</html>
